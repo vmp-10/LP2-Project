b3653945f4b7489e15a08c0d040620ef73266d7e
Fernando Alonso Peral <alonso@aizzap.com>
2024-12-08 18:10:05 +0100

    Created the savetofile class. this class creates a list where the contents will be stored. it then has a construction which creates a new arraylist called contents.
    add content is a method where a string is passed. this string is then added to contents.
    
    then there is another method calle saveToFile where the desired filename is passed. this saves the contents to a file.

--- src/common/SaveToFile.java
+++ src/common/SaveToFile.java
@@ -1,52 +1,37 @@
 package common;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.logging.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SaveToFile {
-    private static final Logger gameLogger = LoggingManager.getInstance().getLogger();
-    private static MemoryHandler memoryHandler;
 
-    static {
-        try {
-            ConsoleHandler consoleHandler = new ConsoleHandler();
-            consoleHandler.setLevel(Level.ALL);
-            consoleHandler.setFormatter(new SimpleFormatter());
+    private List<String> contents;
+    // creating the list
 
-            memoryHandler = new MemoryHandler(consoleHandler, 1000, Level.ALL);
 
-            // Add handlers to the logger
-            gameLogger.addHandler(memoryHandler);
-            gameLogger.addHandler(consoleHandler);
-
-            // Set logger level and disable parent handlers
-            gameLogger.setLevel(Level.ALL);
-            gameLogger.setUseParentHandlers(false);
-        } catch (Exception e) {
-            gameLogger.log(Level.SEVERE, "There was a game error during logger initialization!", e);
-        }
+    // creating the constructino.
+    public SaveToFile() {
+        contents = new ArrayList<>();
     }
 
-    // Public method to retrieve the logger instance
-    public static Logger getLogger() {
-        return gameLogger;
+    // save content to the list
+    public void addContent(String content) {
+        contents.add(content);
     }
 
-    // Method to save logs to a file
-    public static void saveLogsToFile() {
-        FileHandler fileHandler = null;
-        try {
-            // Create a FileHandler to write logs to the specified file
-            fileHandler = new FileHandler("game_record.log", true);
-            fileHandler.setFormatter(new SimpleFormatter());
-
-            // Temporarily add the file handler to the logger
-
-            memoryHandler.push();
+    // method that when called saves the list content into the desired filename
+    public void saveToFile(String filename) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (String line : contents) {
+                writer.write(line);
+                writer.newLine();
+            }
+            System.out.println("Contents saved to file: " + filename);
         } catch (IOException e) {
-            gameLogger.log(Level.SEVERE, "Failed to save logs to file: ", e);
-        } finally {
-            // Ensure the FileHandler is closed
+            System.err.println("Error writing to file: " + e.getMessage());
         }
     }
 }
